6cf4e325fa7b31fd47724665ec919f23251dddf7
Eric <emjyoon@gmail.com>
2021-07-28 14:40:37 -0700

    WIP

--- DiscordImpersonator/config-TEMPLATE.py
+++ DiscordImpersonator/config-TEMPLATE.py
@@ -1,7 +1,7 @@
 BOT_TOKEN = "" # Copy your bot token from the Discord Developers Dashboard here
 
-DATA_CHANNELS = [ # Copy the IDs of each channel to use as training data here
+DATA_CHANNELS = [ # Copy the IDs of each channel to use as training data here [as ints]
 ]
 
-USERS = [ # Copy the IDs of all users you want to train models for here
+USERS = [ # Copy the IDs of all users you want to train models for here [as ints]
 ]
--- DiscordImpersonator/main.py
+++ DiscordImpersonator/main.py
@@ -1,18 +1,70 @@
 import discord
 from discord.ext import commands
 import os
+import json
+import markovify
 from config import *
 
 filedir = os.path.dirname(os.path.realpath(__file__))
 
 bot = commands.Bot(command_prefix="sus ", case_insensitive=True, max_messages=5000)
-bot.run(BOT_TOKEN)
+model_cache = {}
+
+def load_model_from_file(user_id):
+    path = os.path.join(filedir, "data", "models", str(user_id))
+    if not os.path.exists(path):
+        return False
+    with open(path, "r") as file:
+        model_cache[user_id] = markovify.NewlineText.from_json(file.read())
+    return True
+
+def train_model(user_id):
+    training_data = ""
+    file_list = os.listdir(os.path.join(filedir, "data", "channels"))
+    for filename in file_list:
+        with open(os.path.join(filedir, "data", "channels", filename), "r") as file:
+            lines = file.readlines()
+            for line in lines:
+                delimiter = line.find("\t")
+                if delimiter < 0:
+                    continue
+                if str(user_id) != line[:delimiter]:
+                    continue
+                training_data += line[(delimiter + 1):] + "\n"
+    model = markovify.NewlineText(training_data)
+    model_cache[user_id] = model
+    with open(os.path.join(filedir, "data", "models", str(user_id)), "w") as file:
+        file.write(model.to_json())
+
+@bot.event
+async def on_ready():
+    print("Connected to Discord!")
 
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def download(ctx):
-    for channel in DATA_CHANNELS:
-        ctx.send("Downloading channel " + channel + "...")
-        with open(os.path.join(filedir, "data", "channels", channel), "w") as file:
-            async for msg in ctx.channel.history(limit=None):
-                file.write(f"{msg.user.id}\t{msg.clean_content}\n")
+    for channel_id in DATA_CHANNELS:
+        await ctx.send(f"Downloading channel {str(channel_id)}...")
+        channel = bot.get_channel(channel_id)
+        with open(os.path.join(filedir, "data", "channels", str(channel_id)), "w") as file:
+            async for msg in channel.history(limit=None): # TODO: limit=None does not work
+                content = msg.clean_content.replace("\n", " ")
+                file.write(f"{msg.author.id}\t{content}\n")
+    await ctx.send("All done!")
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def train(ctx):
+    for user_id in USERS:
+        await ctx.send(f"Generating model for {str(user_id)}...")
+        train_model(user_id)
+        await ctx.send("All done!")
+
+@bot.command()
+async def run(ctx, *, member:discord.Member):
+    if not member.id in model_cache:
+        if not load_model_from_file(member.id):
+            train_model(member.id)
+    model = model_cache[member.id]
+    await ctx.send(model.make_sentence())
+bot.run(BOT_TOKEN)
